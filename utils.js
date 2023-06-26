07a2354f33af540c25bf9a80e9871a72d24a900f
Shiraz Esat <shiraz.esat@gmail.com>
2023-06-26 16:37:07 +0100

    Function/Tests for escaping DN parts
    https://learn.microsoft.com/en-us/previous-versions/windows/desktop/ldap/distinguished-names
    We're using quoted RDNs though

--- utils.js
+++ utils.js
@@ -1,1 +1,15 @@
-export function doSomething() {}
+export function escapeDNPartQuoted(inString) {
+    const escapeIt = inString.search(/[,=+<>\\#;"]/g);
+    if (escapeIt < 0) return inString;
+  
+    const escaped = inString
+    //   .replace(/\\/g, "\\\\")
+    //   .replace(/\"/g, '\\"')
+      .replace(/\\/g, "_SLASH_IN_OID_")
+      .replace(/\"/g, "_QUOTE_IN_OID_")
+      .replace(/\\/g, "")
+      .replace(/_SLASH_IN_OID_/g, "\\\\")
+      .replace(/_QUOTE_IN_OID_/g, '\\"');
+  
+    return `"${escaped}"`;
+  }
--- utils.test.js
+++ utils.test.js
@@ -1,7 +1,23 @@
-import { doSomething } from "./utils";
+import { escapeDNPartQuoted } from "./utils";
 
-describe("soSomething", () => {
-  it("should return undefined", () => {
-    expect(doSomething()).toBeUndefined();
+describe("escapeDNPartQuoted", () => {
+  it("should return input if no special chars", () => {
+    expect(escapeDNPartQuoted("simple string")).toBe("simple string");
+  });
+
+  it("should quote simple DNs that have special chars", () => {
+    expect(escapeDNPartQuoted("with a comma ,")).toBe('"with a comma ,"');
+  });
+
+  it("should quote and escape DNs that have double quotes", () => {
+    expect(escapeDNPartQuoted('with a "quote"')).toBe('"with a \\"quote\\""');
+  });
+
+  it("should quote and escape DNs that have escape chars", () => {
+    expect(escapeDNPartQuoted('with an \\escape\\')).toBe('"with an \\\\escape\\\\"');
+  });
+
+  it("should handle DNs that have escape quotes", () => {
+    expect(escapeDNPartQuoted('with an \"escape\"')).toBe('"with an \\\"escape\\\""');
   });
 });
